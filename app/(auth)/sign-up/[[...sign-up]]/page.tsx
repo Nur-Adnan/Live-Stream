fc0348dcf5e4090406a21926d542fe418369bdb2
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-12-23 04:15:11 +0600

    updated

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
-import { SignUp } from "@/components/auth/sign-up";
- 
+import { SignIn as SignUp } from "@/components/auth/sign-up";
+
 export default function Page() {
   return <SignUp />;
-};
+}
